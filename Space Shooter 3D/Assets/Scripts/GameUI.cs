375ebd060303b3cb15c4a75def4ed1cfbafb842e
Nidhi Kargathra <nidhikargathra@gmail.com>
2019-04-30 22:30:42 -0400

    resolved rigidbody issues with asteroid andplayer

--- Space Shooter 3D/Assets/Scripts/GameUI.cs
+++ Space Shooter 3D/Assets/Scripts/GameUI.cs
@@ -9,34 +9,34 @@ public class GameUI : MonoBehaviour {
 
     private void Start()
     {
-        ShowMainMenu();
+        ShowGameUI();
     }
-    private void OnEnable()
-    {
-        EventManager.onStartGame += ShowGameUI;
-        EventManager.onPlayerDeath += DelayMainMenuDisplay;
-    }
+    //private void OnEnable()
+    //{
+    //    EventManager.onStartGame += ShowGameUI;
+    //    EventManager.onPlayerDeath += DelayMainMenuDisplay;
+    //}
 
-    private void OnDisable()
-    {
-        EventManager.onStartGame -= ShowGameUI;
-        EventManager.onPlayerDeath -= DelayMainMenuDisplay;
-    }
+    //private void OnDisable()
+    //{
+    //    EventManager.onStartGame -= ShowGameUI;
+    //    EventManager.onPlayerDeath -= DelayMainMenuDisplay;
+    //}
 
-    void DelayMainMenuDisplay()
-    {
-        Invoke("ShowMainMenu", Asteroid.destructionDelay * 3);
-    }
+    //void DelayMainMenuDisplay()
+    //{
+    //    Invoke("ShowMainMenu", Asteroid.destructionDelay * 3);
+    //}
 
-    void ShowMainMenu()
-    {
-        mainMenu.SetActive(true);
-        gameUI.SetActive(false);
-    }
-    
+    //void ShowMainMenu()
+    //{
+    //    //mainMenu.SetActive(true);
+    //    gameUI.SetActive(false);
+    //}
+
     void ShowGameUI()
     {
-        mainMenu.SetActive(false);
+        //mainMenu.SetActive(false);
         gameUI.SetActive(true);
         //Instantiate(playerPrefab, playerStartPosition.transform.position, playerStartPosition.transform.rotation);
     }
